71dad11cf1b1de8d1492c8c9f126e4b70853351b
Noman Ahmed <nomanpb234@gmail.com>
2023-07-23 23:23:26 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//Write a program to explain get set accessor.
+
+namespace Lab_6q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           getSet s1 = new getSet();
+            Console.WriteLine("Enter your Name: ");
+            s1.name = Console.ReadLine();
+            Console.WriteLine("Set your Password: ");
+            string s2 = Console.ReadLine();
+            s1.setting(s2);
+            Console.WriteLine("your name is {0} and Your new Password is {1}.",s1.name,s1.getting());
+        }
+    }
+}
---
+++ getSet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6q1
+{
+    internal class getSet
+    {
+        public string name;
+        private string pass;
+        public string getting()
+        {
+            return pass;
+        }
+        public void setting(string _pass)
+        {
+            pass = _pass;
+        }
+    }
+}
